6516c2049f33ac99bcd37cf105dba5e040c0284d
annandajacobs <annandajacobs@gmail.com>
2024-12-20 15:06:23 -0300

    commit change

--- reconhecimento/views.py
+++ reconhecimento/views.py
@@ -12,13 +12,13 @@ def register_faces(request):
             return JsonResponse({'error': 'Name is required'}, status=400)
         capture_faces_for_person(name)
         return JsonResponse({'message': f'Faces registered for {name}.'})
-    return render(request, 'reconhecimento/recognize_faces.html')
+    return render(request, 'cam_pasta/recognize_faces.html')
 
 def recognize_faces(request):
     if request.method == 'POST':
         capture_and_identify_faces()  # Lógica para iniciar a câmera
         return JsonResponse({'message': 'Face recognition completed.'})
-    return render(request, 'reconhecimento/recognize_faces.html')
+    return render(request, 'cam_pasta/recognize_faces.html')
 
 def reconhecimento_home(request):
     return HttpResponse("Bem-vindo à página de reconhecimento!")
